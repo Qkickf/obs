2307ff381d73ea398ff954dc5c61625bf2230063
yakup furkan cömert <yakupfurkancomert@gmail.com>
2021-11-07 14:58:26 +0300

    Proje ilk versiyon

---
+++ src/OgrenciTest.java
@@ -0,0 +1,24 @@
+public class OgrenciTest {
+    public static void main(String[] args) {
+        //Java'da obje oluşturma bu şekilde
+        Ogrenci ogr = new Ogrenci();
+
+        ogr.setNo(1);
+        ogr.setAd("furkan");
+        ogr.setSoyad("cömert");
+        ogr.setDogumYeri("Rize");
+        ogr.setHarc(1500);
+
+        System.out.println(ogr.getNo() + "-" +ogr.getAd() + " "+ ogr.getSoyad()+ " "+ogr.getHarc());
+
+        ogr.setHarc(2000);
+
+        System.out.println(ogr.getNo()+ "-"+ ogr.getAd()+ " "+ ogr.getSoyad()+ " "+ ogr.getHarc());
+
+        ogr.setHarc(-1000);
+
+        System.out.println(ogr.getNo()+ "-"+ ogr.getAd()+ " "+ ogr.getSoyad()+ " "+ ogr.getHarc());
+
+
+    }
+}
